1d71d99b5301184b30673b93c6ce06e836c211e1
unknown <unknown@unknown.com>
2013-07-18 15:21:04 +0800

    update heart2

--- heart2/heart.js
+++ heart2/heart.js
@@ -63,18 +63,22 @@ setInterval(function(_) { // The main draw loop. _ is unused; just here to match
     
     // Rotate the point in 3D and draw the circle
     l=Math.sin(Z=t+3*o.pageY/H)*m[i].y+Math.cos(Z)*m[i][j], // Used in 3D rotation calc
-    a.fill(a.arc( // Stick arc in the free semicolon zone
+    
+            a.beginPath();
+            a.arc( // Stick arc in the free semicolon zone
       (Math.sin(y=.7*t+3*o.pageX/W)*l+Math.cos(y)*m[i].x)*25+W/2, // Point x
       H/2+(Math.cos(Z)*m[i].y-Math.sin(Z)*m[i][j])*25, // Point y
       Math.max(0,12-(Math.cos(y)*l-Math.sin(y)*m[i].x)/2.5), // Point size/z
-      Math.max(0,6-t),6)), // Controls the fade in at the beginning
+      Math.max(0,6-t),6);
+            a.closePath();
+            a.fill(), // Controls the fade in at the beginning
 
     // Set the text color. Its smaller to use rgba here at first glance, but hsla saves _lots_ of bytes because it lets us repeat stuff, and jscrush likes that
     // Use .51 because with .5 we could get numbers in scientific notation, which hsla doesn't allow
     a.fillStyle="hsla("+[332,"97%,47%",.51-Math.cos(t/2)/2]+")",
     // Draw in the flowers/hearts floating around
-    i%41||a.fillText("❁✿♡❤"[3&t/4/Math.PI],W/2+Math.sin(t/8+i)*W/2,H/2+Math.cos(t/8+i*9)*Math.sin(t/2)*H/2)
+    i%41||1//a.fillText("❁✿♡❤"[3&t/4/Math.PI],W/2+Math.sin(t/8+i)*W/2,H/2+Math.cos(t/8+i*9)*Math.sin(t/2)*H/2)
   }
   // Finally, draw in the poem itself!
-  a.fillText(["roses are red","violets are blue","i ♥ js1k","and so should you"][3&t/4/Math.PI],200+W/2,H/2+Math.pow(7*Math.cos(t/4),3))
-}, w=16) // Aim for 16 fps, also define w (morph offset, value doesn't matter yet)
+//  a.fillText(["roses are red","violets are blue","i ♥ js1k","and so should you"][3&t/4/Math.PI],200+W/2,H/2+Math.pow(7*Math.cos(t/4),3))
+copy();}, w=16) // Aim for 16 fps, also define w (morph offset, value doesn't matter yet)
